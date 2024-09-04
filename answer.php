5ab4f46090d239fbada1336f7426732f6538d76e
PRANKUR PATHAK <prankurpathak6@gmail.com>
2024-09-04 22:08:09 +0530

    Update answer.php

--- answer.php
+++ answer.php
@@ -12,16 +12,21 @@ $correct_answer = $_POST['correct_answer'];
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h2>You selected: <?php echo htmlspecialchars($selected_answer); ?></h2>
-    <h3>
-        <?php
-        if ($selected_answer === $correct_answer) {
-            echo "Correct! The right answer is: " . htmlspecialchars($correct_answer);
-        } else {
-            echo "Incorrect! The correct answer was: " . htmlspecialchars($correct_answer);
-        }
-        ?>
-    </h3>
-    <a href="index.php">Go Back to Quiz Start</a>
+    <div class="result-container">
+        <h2>Your Answer:</h2>
+        <p class="answer"><?php echo htmlspecialchars($selected_answer); ?></p>
+        
+        <h3>
+            <?php
+            if ($selected_answer === $correct_answer) {
+                echo "Correct! The right answer is: <span class='correct-answer'>" . htmlspecialchars($correct_answer) . "</span>";
+            } else {
+                echo "That's Incorrect! The correct answer was: <span class='correct-answer'>" . htmlspecialchars($correct_answer) . "</span>";
+            }
+            ?>
+        </h3>
+        
+        <button onclick="window.location.href='index.php'">Go Back to Quiz Start</button>
+    </div>
 </body>
 </html>
